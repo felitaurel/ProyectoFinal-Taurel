3e436b56185dbff55079300d2299b77d0897309f
feli <felitaurel@gmail.com>
2023-05-20 13:21:28 -0300

    correcion de errores y muestra de datos de empleado

--- preentrega2/main.js
+++ preentrega2/main.js
@@ -26,20 +26,20 @@ class Empleados {
     let persona = [nombre, edad, puesto, salario]
     arregloEmpleados.push(persona)
     console.log("El empleado " + nombre + " cobra " + salario + " pesos al mes.")
-    /*if (confirm("Quiere poner un aumento al salario?")){
+    if (confirm("Quiere poner un aumento al salario?")){
         cantidad = parseFloat(prompt('Cuanto quiere aumentar? (en porcentaje)'))
         empleado.aumento(cantidad) 
         console.log("El empleado" + empleado.nombre + " ahora cobra " + empleado.salario) 
-    }*/ 
-    continuar = confirm("quiere seguir agregando empleados")
+    } 
+    continuar = confirm("Quiere seguir agregando empleados?")
 
     
   }
 
-  if (confirm("Quiere sacar algun empleado?")){
+  if (confirm("Quiere despedir algun empleado?")){
     
     
-    despedido = prompt("escriba el primer nombre del empleado")
+    despedido = prompt("Escriba el primer nombre del empleado")
     for (var i = 0; i < arregloEmpleados.length; i++){
         if (arregloEmpleados[i][0].indexOf(despedido) != -1){
             arregloEmpleados.splice(i, 1)
@@ -48,3 +48,13 @@ class Empleados {
     console.log("El empleado " + despedido + " ha sido despedido")
 }
 console.log(arregloEmpleados);
+
+
+if (confirm("Quiere algun dato de un empleado?")){ // aca podria preguntarle que dato especifico del empleado quiere mediante un prompt y devolver solo ese dato
+    datos = prompt("Escriba el primer nombre del empleado que quiere analizar")
+    for (var i = 0; i < arregloEmpleados.length; i++){
+        if (arregloEmpleados[i][0].indexOf(datos) != -1){
+            console.log(`El empleado ${arregloEmpleados[i][0]} tiene ${arregloEmpleados[i][1]} años y un sueldo de ${arregloEmpleados[i][3]} pesos en su puesto de ${arregloEmpleados[i][2]}`)
+        }   
+    }
+}
