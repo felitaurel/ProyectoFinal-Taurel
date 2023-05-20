1e25c91a24706d0a7c832a82cd71da480307d2ed
feli <felitaurel@gmail.com>
2023-05-20 13:06:48 -0300

    despido de empleados con nombre y splice

--- preentrega2/main.js
+++ preentrega2/main.js
@@ -22,14 +22,29 @@ class Empleados {
     const salario = parseInt(prompt('Ingrese el salario'))
     
     const empleado = new Empleados(nombre, edad, puesto, salario)
-    arregloEmpleados.push(empleado)
+    
+    let persona = [nombre, edad, puesto, salario]
+    arregloEmpleados.push(persona)
     console.log("El empleado " + nombre + " cobra " + salario + " pesos al mes.")
-    if (confirm("Quiere poner un aumento al salario?")){
+    /*if (confirm("Quiere poner un aumento al salario?")){
         cantidad = parseFloat(prompt('Cuanto quiere aumentar? (en porcentaje)'))
         empleado.aumento(cantidad) 
         console.log("El empleado" + empleado.nombre + " ahora cobra " + empleado.salario) 
-    } 
+    }*/ 
     continuar = confirm("quiere seguir agregando empleados")
+
+    
   }
 
-console.log(arregloEmpleados)
+  if (confirm("Quiere sacar algun empleado?")){
+    
+    
+    despedido = prompt("escriba el primer nombre del empleado")
+    for (var i = 0; i < arregloEmpleados.length; i++){
+        if (arregloEmpleados[i][0].indexOf(despedido) != -1){
+            arregloEmpleados.splice(i, 1)
+        }   
+    }
+    console.log("El empleado " + despedido + " ha sido despedido")
+}
+console.log(arregloEmpleados);
